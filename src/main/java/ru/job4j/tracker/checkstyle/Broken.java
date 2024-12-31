bf76de40d8e77f52833195f9eebdf4e11fd27b13
dmitrii <dbalan@okta-solutions.com>
2024-12-31 12:05:17 +0300

    6. Правила Checkstyle

--- src/main/java/ru/job4j/tracker/checkstyle/Broken.java
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -2,22 +2,19 @@ package ru.job4j.tracker.checkstyle;
 
 public class Broken {
     private int sizeOfEmpty = 10;
+    private String surname;
+    private String name;
+    private static final String NEW_VALUE = "";
 
-    public String surname;
+    public Broken() { }
 
-    public static final String NEW_VALUE = "";
+    public void echo() { }
 
-    void echo() { }
-
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) { }
-
-    String name;
-
-    Broken() { }
+    public void method(int a, int b, int c, int d, int e, int f, int g) { }
 }
