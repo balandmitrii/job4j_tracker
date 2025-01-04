f725708c6f57f675f43df5498541f07f9867adb8
dmitrii <dbalan@okta-solutions.com>
2025-01-04 21:33:13 +0300

    6. Метод замены заявки. Tracker.replace

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,16 +13,29 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+    public boolean replace(int id, Item item) {
+        Item foundItem = findById(id);
+        if (foundItem != null) {
+            foundItem.setName(item.getName());
+            return true;
+        }
+        return false;
+    }
+
+    public int indexOf(int id) {
+        int result = -1;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                result = i;
                 break;
             }
         }
-        return rsl;
+        return result;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
