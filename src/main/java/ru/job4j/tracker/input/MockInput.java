ac08ab148e42cc1e03b5030d04f7e346855fcf7d
dmitrii <dbalan@okta-solutions.com>
2025-01-11 12:14:30 +0300

    1.1. Тесты на StartUI

--- src/main/java/ru/job4j/tracker/input/MockInput.java
+++ src/main/java/ru/job4j/tracker/input/MockInput.java
@@ -1,7 +1,7 @@
 package ru.job4j.tracker.input;
 
 public class MockInput implements Input {
-    private String[] answers;
+    private final String[] answers;
     private int position = 0;
 
     public MockInput(String[] answers) {
