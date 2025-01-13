16cf76074633505b7100389b4409bc91e82c5861
dmitrii <dbalan@okta-solutions.com>
2025-01-13 10:10:11 +0300

    1. Singleton

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,16 +1,14 @@
 package ru.job4j.tracker;
 
 public class SingleTracker {
-    private static Tracker tracker = null;
+    private static final SingleTracker INSTANCE = new SingleTracker();
+    private final Tracker tracker = new Tracker();
 
     private SingleTracker() {
     }
 
-    public static Tracker getInstance() {
-        if (tracker == null) {
-            tracker = new Tracker();
-        }
-        return tracker;
+    public static SingleTracker getInstance() {
+        return INSTANCE;
     }
 
     public Item add(Item item) {
