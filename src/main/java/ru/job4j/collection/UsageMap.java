dbb15fe0fdbe247ebceade7e27772837e64a8b9c
dmitrii <dbalan@okta-solutions.com>
2025-01-14 16:28:51 +0300

    0. Map, HashMap

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("balandmitry.webdev@gmail.com", "Balan Dmitriy Petrovich");
+        hashMap.put("ChuckNorris@gmail.com", "Chuck Norris");
+        hashMap.put("BruceLee@gmail.com", "Bruce Lee");
+        for (Map.Entry<String, String> item : hashMap.entrySet()) {
+            System.out.println("Email: " + item.getKey() + ", name: " + item.getValue());
+        }
+    }
+}
