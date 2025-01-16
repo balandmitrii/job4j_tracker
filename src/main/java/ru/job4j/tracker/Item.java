1442772685ff1cd7508476c79f402bc9cf399eee
dmitrii <dbalan@okta-solutions.com>
2025-01-16 14:40:52 +0300

    0. Сортировка

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -53,6 +53,9 @@ public class Item {
 
     @Override
     public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
