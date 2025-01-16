665dc791db5603c336d2ff2264365c8975463501
dmitrii <dbalan@okta-solutions.com>
2025-01-16 11:23:54 +0300

    3. Банковские переводы

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -21,8 +21,12 @@ public class BankService {
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
-        if (users.get(user) != null && !users.get(user).contains(account)) {
-            users.get(user).add(account);
+        if (user == null) {
+            return;
+        }
+        List<Account> accounts = users.get(user);
+        if (!accounts.contains(account)) {
+            accounts.add(account);
         }
     }
 
