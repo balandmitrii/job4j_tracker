d390ab9b4bb97a652e776861feaa9e07a285573e
dmitrii <dbalan@okta-solutions.com>
2025-01-18 12:27:15 +0300

    4. Сортировка номера

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        List<String> splitLeft = List.of(left.split(". "));
+        List<String> splitRight = List.of(right.split(". "));
+        int compareNumberPart = Integer.compare(
+                Integer.parseInt(splitLeft.get(0)),
+                Integer.parseInt(splitRight.get(0))
+        );
+        return compareNumberPart == 0 ? left.compareTo(right) :
+                compareNumberPart;
+    }
+}
