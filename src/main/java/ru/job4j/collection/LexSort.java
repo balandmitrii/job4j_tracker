07326b5c4b404a979b4ca8035749defa58862c41
dmitrii <dbalan@okta-solutions.com>
2025-01-20 09:40:20 +0300

    4. Сортировка номера

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        List<String> splitLeft = Arrays.asList(left.split("\\."));
-        List<String> splitRight = Arrays.asList(right.split("\\."));
+        String[] splitLeft = left.split("\\.");
+        String[] splitRight = right.split("\\.");
         int compareNumberPart = Integer.compare(
-                Integer.parseInt(splitLeft.get(0)),
-                Integer.parseInt(splitRight.get(0))
+                Integer.parseInt(splitLeft[0]),
+                Integer.parseInt(splitRight[0])
         );
         return compareNumberPart == 0
                 ? left.compareTo(right)
