7a06ca4b99940152336ad0cf462e21d4ab3f7a2e
dmitrii <dbalan@okta-solutions.com>
2025-01-20 21:04:42 +0300

    7. Функции высшего порядка

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,31 +1,53 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
+/**
+ * Класс PhoneDictionary предназначен для поиска людей в списке по различным критериям.
+ */
 public class PhoneDictionary {
+
+    /**
+     * Список, содержащий объекты типа Person.
+     */
     private final ArrayList<Person> persons = new ArrayList<>();
 
+    /**
+     * Добавляет нового человека в словарь.
+     *
+     * @param person Объект типа Person для добавления.
+     */
     public void add(Person person) {
         this.persons.add(person);
     }
 
     /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     * @param key Ключ поиска.
-     * @return Список пользователей, которые прошли проверку.
+     * Выполняет поиск людей по заданному ключу. Поиск осуществляется по имени, фамилии, номеру телефона и адресу.
+     *
+     * @param key Ключевое слово для поиска в информации о людях.
+     * @return Список объектов типа Person, соответствующих критериям поиска.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> byPhone = (person) -> person.getPhone().contains(key);
+        Predicate<Person> byAddress =
+                (person) -> person.getAddress().contains(key);
+        Predicate<Person> byName =
+                (person) -> person.getName().contains(key);
+        Predicate<Person> bySurname =
+                (person) -> person.getSurname().contains(key);
+
+        Predicate<Person> combine =
+                byPhone.or(byAddress).or(byName).or(bySurname);
+
         ArrayList<Person> result = new ArrayList<>();
+
         for (Person person : persons) {
-            if (
-                    person.getPhone().contains(key)
-                    || person.getAddress().contains(key)
-                    || person.getName().contains(key)
-                    || person.getSurname().contains(key)
-            ) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
+
         return result;
     }
 }
