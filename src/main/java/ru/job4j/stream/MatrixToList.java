25234fb48568552dd3de7e200513c5cef145d63e
dmitrii <dbalan@okta-solutions.com>
2025-01-24 17:24:34 +0300

    удалить лишние импорты

--- src/main/java/ru/job4j/stream/MatrixToList.java
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -1,11 +1,8 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MatrixToList {
 
