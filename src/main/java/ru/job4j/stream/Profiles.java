f5d2889c4dd003e98c223fc1c0e5b7e64ae76c3d
dmitrii <dbalan@okta-solutions.com>
2025-01-25 08:13:12 +0300

    7. Уникальность элементов и сортировка.

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -16,7 +16,7 @@ public class Profiles {
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
-                .map((Profile profile) -> profile.getAddress())
+                .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .toList();
