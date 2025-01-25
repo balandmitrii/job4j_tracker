be613ae37d4439281d47ac7a42bd6d1440ac4281
dmitrii <dbalan@okta-solutions.com>
2025-01-25 08:11:16 +0300

    6. Список адресов

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -10,7 +10,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map((Profile profile) -> profile.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
